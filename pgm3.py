5a06917b2decf874516613c528f8c711ac2cba8d
aafreen22 <55572038+aafreen22@users.noreply.github.com>
2019-10-30 22:53:47 +0530

    Update pgm3.py

--- pgm3.py
+++ pgm3.py
@@ -1,21 +1,11 @@
 """takes numbers as input and searches an element and returns the indices if it is present in the list"""
-
 a = []
-
 n = int(input("Enter the number of elements you want to input: "))
-
 for i in range(n):
-
 	a.append(int(input())) #takes input for a fixed number of elements
-
 k = int(input("Enter your search key:: ")) #takes the search element from user
-
 b = []
-
 for i, j in enumerate(a): #enumerate returns all occurences
-
     if j == k:
-
         b.append(i)
-
 print(b)
