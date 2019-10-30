3184abfe217ebe0dc83123e10c955a89671eb155
aafreen22 <55572038+aafreen22@users.noreply.github.com>
2019-10-30 22:55:34 +0530

    Update pgm5.py

--- pgm5.py
+++ pgm5.py
@@ -1,17 +1,9 @@
 # this program asks the user for a string and displays it in reverse order
-
 str = input("Enter a string:: ")
-
 list = str.split() #converts the string to a list
-
 rev = list[::-1] #reverses the list
-
 resu = ""
-
 for val in rev:
-
 	resu = resu + " " + val #creates a string from the reverse of the list
-
 res = resu[1::] #removes first character as it is a space
-
 print(res.capitalize()) #capitalizes the first letter of the string
