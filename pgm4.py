c3dae3e38536d0708a28b5621ecf2019c7b1973a
aafreen22 <55572038+aafreen22@users.noreply.github.com>
2019-10-30 22:54:40 +0530

    Update pgm4.py

--- pgm4.py
+++ pgm4.py
@@ -1,19 +1,10 @@
 """takes a list as input and returns a new list with unique elements from the list"""
-
 a = []
-
 n = int(input("Enter the number of elements you want to input: "))
-
 for i in range(n):
-
 	a.append(int(input())) #takes input for a fixed number of elements
-
 b = []
-
 for val in a:
-
 	if val not in b: #checks if value is not in b and add it if it is unique
-
 		b.append(val)
-
 print(b)
