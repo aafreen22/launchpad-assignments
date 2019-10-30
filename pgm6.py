d5d296924d821e99c7d8e50de92052fd048fd9a5
aafreen22 <55572038+aafreen22@users.noreply.github.com>
2019-10-30 22:56:17 +0530

    Update pgm6.py

--- pgm6.py
+++ pgm6.py
@@ -1,13 +1,7 @@
 #this program takes 3 entries for dictionary and prints the dictionary
-
 records = dict()
-
 for i in range(3):
-
 	name = input("Enter Name: ")
-
 	usn = input("Enter USN: ")
-
 	records[usn] = name
-
 print(records)
